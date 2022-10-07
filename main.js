28e0aebd8db98419b6657b7af8a195da75506aa1
Shuxrat Mamedov <lider_20_10@mail.ru>
2022-10-07 23:34:18 +0300

    доделал логику, добавил анимацию

--- main.js
+++ main.js
@@ -1,20 +1,43 @@
 const listItems = document.querySelectorAll('.list-item')
+const listBox = document.querySelector('.list-box')
+const btn = document.querySelector('.btn')
+const btnContainer = document.querySelector('.box')
+
 let arrIndex = []
 
 let clearItem = function () {
     arrIndex.forEach((item, index) => {
         setTimeout(() => {
-            item.classList.toggle('clicked')
+            listItems[item].classList.toggle('clicked')
         }, 1000 * (index + 1))
+        arrIndex = []
     })
 }
 
-listItems.forEach(function (items) {
+listItems.forEach(function (items, index) {
     items.addEventListener('click', function (event) {
-        arrIndex.push(event.target)
         items.classList.add('clicked')
+        arrIndex.push(index)
         if (listItems.length === arrIndex.length) {
             clearItem()
         }
     })
 })
+
+
+btn.addEventListener('click', function () {
+    animate()
+})
+let marginPlus = -100
+
+function animate() {
+    marginPlus += 1.5
+
+    btnContainer.style.display = 'none'
+    listBox.style.marginTop = `${marginPlus}%`
+    if (marginPlus < 0) {
+        requestAnimationFrame(animate)
+    }
+}
+
+
